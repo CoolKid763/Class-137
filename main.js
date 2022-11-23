f526e47e51d8868a9f0a21aff272a7505cb9456d
CoolKid763 <73192943+CoolKid763@users.noreply.github.com>
2022-11-23 14:34:33 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,28 +1,27 @@
 objects = [];
-video = "";
 status = "";
 
 function preload(){
     video = createVideo('video.mp4');
-    video.hide();
 }
 
 function setup(){
     canvas = createCanvas(480, 380);
     canvas.center();
+    video.hide();
 }
 
 function draw(){
     image(video, 0, 0, 480, 380);
     if(status != ""){
         objectDetector.detect(video, gotResult);
-        for (i=0; i<=objects.length; i++){
+        for (i=0; i<objects.length; i++){
             document.getElementById("status").innerHTML = "Status : Objects Detected"
             document.getElementById("number_of_objects").innerHTML = "NUmber of objects detected are : "+ objects.length;
 
             fill("#FF0000");
             percent = floor(objects[i].confidence * 100);
-            text(objects[i].label + " "+ percent + "%", objects[i].x + 15,objects[i].y + 15 )
+            text(objects[i].label + " "+ percent + "%", objects[i].x + 15,objects[i].y + 15 );
             noFill();
             stroke("#FF0000");
             rect(objects[i].x,objects[i].y, objects[i].width, objects[i].height);
@@ -40,11 +39,11 @@ function gotResult(){
 
 function start(){
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
-    document.getElementById("status").innerHTML = "status : Detecting Objects"
+    document.getElementById("status").innerHTML = "status : Detecting Objects";
 }
 
 function modelLoaded(){
-    console.log("Model Loaded!");
+    console.log("Model Loaded!")
     status = true;
     video.loop();
     video.speed(1);
